f75400dd8ffe245b11add0a62f555a5bd32a8618
edgar-yesid-sanchez <edgaryesidsanchezbueno@gmail.com>
2020-08-21 21:56:43 -0500

    0.01

---
+++ Scripts/script.js
@@ -0,0 +1,7 @@
+var slidePage = document.querySelector(".slidepage");
+var firtNextBtn = document.querySelector(".nextBtn");
+var p1  = document.getElementById('p1');
+
+function desplazar(){
+    p1.style.marginLeft = "-31.5%"
+}
